5af210ae94c01ec8596a19d2b67b62c8bbd535f0
abdelkhaleksaadani <abdelkhaleksaadani@gmail.com>
2024-07-09 23:22:42 +0100

    my ssolution,

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,34 +1,33 @@
-import { useState } from 'react'
-import '../styles/Cart.css'
+import { useState } from "react";
+import "../styles/Cart.css";
 
 function Cart() {
-	const monsteraPrice = 8
-	const [cart, updateCart] = useState(0)
-	const [isOpen, setIsOpen] = useState(true)
+  const monsteraPrice = 8;
+  const [cart, updateCart] = useState(0);
+  const [isOpen, setIsOpen] = useState(true);
 
-	return isOpen ? (
-		<div className='jh-cart'>
-			<button
-				className='jh-cart-toggle-button'
-				onClick={() => setIsOpen(false)}
-			>
-				Close
-			</button>
-			<h2>Cart</h2>
-			<div>Monstera : {monsteraPrice}€</div>
-			<button onClick={() => updateCart(cart + 1)}>Add</button>
-			<h3>Total : {monsteraPrice * cart}€</h3>
-		</div>
-	) : (
-		<div className='jh-cart-closed'>
-			<button
-				className='jh-cart-toggle-button'
-				onClick={() => setIsOpen(true)}
-			>
-				Open Cart
-			</button>
-		</div>
-	)
+  return isOpen ? (
+    <div className="jh-cart">
+      <button
+        className="jh-cart-toggle-button"
+        onClick={() => setIsOpen(false)}
+      >
+        Close
+      </button>
+      <h2>Cart</h2>
+      <div>Monstera : {monsteraPrice}€</div>
+      <button onClick={() => updateCart(cart + 1)}>Add</button>
+      <button onClick={() => updateCart((cart) => cart >0 ?cart - 1:0)}>Remove</button>
+      <h3>Total : {monsteraPrice * cart}€</h3>
+      <button onClick={() => updateCart(0)}>Clear</button>
+    </div>
+  ) : (
+    <div className="jh-cart-closed">
+      <button className="jh-cart-toggle-button" onClick={() => setIsOpen(true)}>
+        Open Cart
+      </button>
+    </div>
+  );
 }
 
-export default Cart
+export default Cart;
