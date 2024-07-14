ec36808e003d27b57beb0643997e1630acfaffd6
abdelkhaleksaadani <abdelkhaleksaadani@gmail.com>
2024-07-14 07:47:30 +0100

    my solution

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,28 @@
-import { plantList } from '../datas/plantList'
+import { plantList } from "../datas/plantList";
 
 function ShoppingList() {
-	return <ul>
-		{plantList.map((plant) => (
-			<li key={plant.category}>{plant.category}</li>
-		))}
-	</ul>
+  const categories = plantList.reduce(
+    (acc, currentPlant) =>
+      acc.includes(currentPlant.category)
+        ? acc
+        : acc.concat(currentPlant.category),
+    []
+  );
+  return (
+    <div>
+      <ul>
+        {categories.map((cat) => (
+          <li key={cat}>{cat}</li>
+        ))}
+      </ul>
+
+      <ul>
+        {plantList.map((plant) => (
+          <li key={plant.id}>{plant.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default ShoppingList
+export default ShoppingList;
